1e9e76c39f909174581e13ef15836deecc636210
barclay loftus <barclay@distinctpixel.com>
2017-12-14 15:13:07 -0800

    Updating meta data and minutiae

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,23 @@
 name             'looker'
 maintainer       'Deliv'
 maintainer_email 'eng@deliv.co'
-license          'all_rights'
+license          'MIT'
 description      'Installs/Configures Looker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 supports         ['ubuntu']
 
+recipe 'looker::default', 'Installs the looker application (calls to `setup`)'
+recipe 'looker::setup',   'Installs the looker application'
+recipe 'looker::user',    'Installs the looker user, group and home directory'
+recipe 'looker::ssl',     'Sets up a custom SSL certificate for the looker application'
+recipe 'looker::stop',    'Stops the looker service'
+recipe 'looker::start',   'Starts the looker service'
+
 source_url 'https://github.com/deliv/looker-cookbook'
 issues_url 'https://github.com/deliv/looker-cookbook/issues'
-chef_version '>= 12.1'
 
 depends 'java'
 depends 'aws'
+
+chef_version '>= 12.1'
